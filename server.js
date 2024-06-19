a643d9129a2ce85679a0edbcc99692624723fb4e
Antoine Janssens <antcool.jeux@gmail.com>
2024-06-19 23:14:23 +0200

    added a home page

--- server.js
+++ server.js
@@ -3,14 +3,21 @@ const { Pool } = require('pg');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
+const path = require('path')
 
 // Load environment variables from .env file
 dotenv.config();
 
-console.log(process.env.PORT)
-
 const app = express();
 const port = process.env.PORT || 8080;
+
+// Serve static files from the 'public' directory   
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define a route to serve the index.html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'home.html'));
+});
 
 app.listen(port, () => {
     console.log(`it's alive on http://localhost:${port}`);
