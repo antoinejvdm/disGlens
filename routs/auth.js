2865bf9c464dbce8e2b7a1a1545591d9219a38dc
Antoine Janssens <antcool.jeux@gmail.com>
2024-07-20 09:15:21 +0200

    auth is working whit db, new structure in db

---
+++ routs/auth.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const bcryptjs = require("bcryptjs");
+const authRouter = express.Router();
+const db = require("../db_conection");
+const jwt = require("jsonwebtoken");
+
+//Sign up rout
+authRouter.post("/api/reggister", async (req, res) => {
+    try {
+        const {user_id, email, password} =  req.body;
+        
+        // Email validation function
+        function isValidEmail(email) {
+            const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+            return emailRegex.test(email);
+        }
+
+        if (!isValidEmail(email)) {
+            return res.status(400).json({ error: 'Invalid email format' });
+            }
+
+        // Check if the email or username already exists
+        const userCheck = await db.query('SELECT * FROM users WHERE email = $1 OR user_id = $2', [email, user_id]);
+        if (userCheck.rows.length > 0) {
+            return res.status(400).json({ error: 'Username or email already exists' });
+        }
+
+        if (!user_id || !email || !password) {
+            return res.status(400).json({ error: 'Username, email, and password are required!' });
+          }
+        
+        // Hash the password
+        const salt = bcryptjs.genSaltSync(9); // You can adjust the salt rounds
+        const hashedPassword = bcryptjs.hashSync(password, salt);
+        
+        // Insert the user into the database
+        const result = await db.query(
+            'INSERT INTO users (user_id, email, password) VALUES ($1, $2, $3) RETURNING *', [user_id, email, hashedPassword]
+        );
+
+        // Send response
+        const user = result.rows[0];
+
+        // create token to stay logd in
+        const token = jwt.sign({user}, "secretkey")
+
+        return res.status(201).json({
+            msg: 'Reggister successful',
+            user: {
+                user_id: user.user_id,
+                email: user.email,
+            },
+            token,
+          });
+
+    } catch (e) {
+        console.error('Error during registration', e);
+        res.status(500).json({ error: 'Somthing whent wrong when registering contact Antoine'});
+    }
+});
+
+//sine in rout
+authRouter.post("/api/login", async (req,res) => {
+try {
+    const {email, password} = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ error: 'email or password are mising!' });
+      }
+
+    // Check if the email exists
+    const emailChek = await db.query('SELECT * FROM users WHERE email = $1', [email]);
+    if (!(emailChek.rows.length > 0)) {
+        return res.status(400).json({ error: 'Email dus not exist create account first' });
+    }
+
+    const user = emailChek.rows[0];
+
+    // Compare hashed password
+    const isMatch = bcryptjs.compareSync(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ error: 'Invalid email or password' });
+    }
+
+    // create token to stay logd in
+    const token = jwt.sign({user}, "secretkey")
+
+    // Send response
+    return res.status(200).json({
+        msg: 'Login successful',
+        user: {
+            user_id: user.user_id,
+            email: user.email,
+          },
+          token,
+      });
+
+} catch (e) {
+    console.error('Error during login', e);
+    res.status(500).json({ error: 'Somthing whent wrong when loging in contact Antoine'});    
+}
+});
+
+
+module.exports = authRouter
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { Pool } = require('pg');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
-const path = require('path')
+const path = require('path');
+const fs = require('fs');
+const authRouts = require("./routs/auth");
 
 // Load environment variables from .env file
 dotenv.config();
@@ -14,6 +15,9 @@ const port = process.env.PORT || 8080;
 // Serve static files from the 'public' directory   
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Parse JSON bodies of incoming requests
+app.use(express.json({ limit: '50mb'}));
+
 // Define a route to serve the index.html file
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'home.html'));
@@ -23,14 +27,69 @@ app.listen(port, () => {
     console.log(`it's alive on http://localhost:${port}`);
 });
 
-// Configure PostgreSQL client
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-});
-  
-// Middleware to parse JSON bodies
-app.use(express.json());
+app.use(authRouts);
+
+// // API endpoint get all users
+// app.get('/api/users', async (req, res) => {
+//   try {
+//     // Query
+//     const query = 'SELECT * FROM users';
+
+//     // Query execution
+//     const { rows } = await pool.query(query);
+
+//     // Send response with all users
+//     res.json(rows);
+//   } catch (error) {
+//     console.error('Error executing query', error);
+//     res.status(500).json({ message: 'Internal server error wile geting users' });
+//   }
+// });
+
+// // API endpoint get all posts
+// app.get('/api/allpost', async (req, res) => {
+//   try {
+//     // Query
+//     const query = 'SELECT * FROM post';
+
+//     // Query execution
+//     const { rows } = await pool.query(query);
+
+//     // Send response with all posests
+//     res.json(rows);
+//   } catch (error) {
+//     console.error('Error executing query', error);
+//     res.status(500).json({ message: 'internal error when geting post' });
+//   }
+// });
+
+// // API endpoint to create a new post indcoded in base64
+// app.post('/api/post', async (req, res) => {
+//   const { user_id, img, comment} = req.body;
+
+//   // Basic validation
+//   if (!user_id || !img) {
+//     return res.status(400).json({ message: 'Missing required fields' });
+//   }
+
+//   try {
+//     // Query to insert a new post into the database
+//     const query = `
+//       INSERT INTO post (user_id, img, comment, time)
+//       VALUES ($1, $2, $3, CURRENT_TIMESTAMP)
+//       RETURNING *;
+//     `;
+//     const values = [user_id, img, comment];
+
+//     // Query execution
+//     const { rows } = await pool.query(query, values);
+
+//     // Send response with the newly created post
+//     res.status(201).json(rows[0]);
+//   } catch (error) {
+//     console.error('Error executing query', error);
+//     res.status(500).json({ message: 'Internal server error wile posting a post' });
+//  }
+
+// }
+// );
